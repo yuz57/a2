cf1390f1bf41e79533e5255abf8edb2237f11b2b
yuz57 <yuzuruprogram@gmail.com>
2026-05-02 17:33:05 +0900

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  // Firebase Console のWebアプリ設定値を入れてください
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
+
+export { firebaseConfig };
+export default firebaseConfig;
